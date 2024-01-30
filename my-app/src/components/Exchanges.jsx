a940825976826607a108dc969e170612b3674a30
AmoushRye <amoskhams1234@gmail.com>
2024-01-30 20:22:23 +0530

    first ui

--- my-app/src/components/Exchanges.jsx
+++ my-app/src/components/Exchanges.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { server } from '../index'
-import { Container, HStack, VStack ,Image} from '@chakra-ui/react'
+import { Container, HStack, VStack, Image, Heading, Text } from '@chakra-ui/react'
 import Loader from "./Loader"
 const Exchanges = () => {
   const [exchanges, setExchanges] = useState([])
@@ -9,29 +9,40 @@ const Exchanges = () => {
   useEffect(() => {
     const fetchExchanges = async () => {
       const { data } = await axios.get(`${server}/exchanges?`)
+      console.log(data)
       setExchanges(data)
       setLoading(false)
     }
     fetchExchanges();
   }, [])
   return (
-    <Container maxW={"container.xl"}>{loading ? (<Loader />) : (<>
-      <HStack wrap={"wrap"}>
-        {exchanges.map((i) => (
-          <ExchangeCard key={i.id} name={i.name} img={i.img} rank={i.trust_score_rank} url={i.url} />
-        ))}
-      </HStack>
-    </>
-    )}
+    <Container maxW={"container.xl"}>
+      {loading ? (<Loader />) : (<>
+        <HStack wrap={"wrap"}>
+          {
+            exchanges.map((i) => (
+              <ExchangeCard key={i.id} name={i.name} img={i.image} rank={i.trust_score_rank} url={i.url} />
+            ))
+          }
+        </HStack>
+      </>
+      )}
     </Container>
   )
 }
-const ExchangeCard = ({ name, img, rank, url }) => (
+
+const ExchangeCard = ({ name, img, rank, url }) =>
   <a href={url} target={"blank"}>
-    <VStack>
-      <Image  src={img} w={"10"} h={10} objectFit={"contain"}  alt={"Exchanges"}/>
-      
+    <VStack w={"52"} shadow={"lg"} p="8" borderRadius={"lg"} transition={"all 0.3s"} m={"4"}
+      css={{
+        "&:hover": {
+          transform: "scale(1.2)"
+        }
+      }}
+    >
+      <Image src={img} w={"10"} h={"10"} objectFit={"contain"} alt={"exchange"} />
+      <Heading size={"md"} noOfLines={1}>{rank}</Heading>
+      <Text noOfLines={1}>{name}</Text>
     </VStack>
   </a>
-);
 export default Exchanges
