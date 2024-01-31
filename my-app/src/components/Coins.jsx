027699e184ffc0e51c483e3f5774f1cc09e5fb9d
AmoushRye <amoskhams1234@gmail.com>
2024-02-01 00:10:25 +0530

    loader spinner complete

--- my-app/src/components/Coins.jsx
+++ my-app/src/components/Coins.jsx
@@ -6,16 +6,17 @@ import Loader from "./Loader"
 import ErrorComponent from './ErrorComponent'
 import CoinCard from './CoinCard'
 const Coins = () => {
-  const [coins,setCoins] = useState([])
+  const [coins, setCoins] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(false);
-  const [page,setPage] = useState(1);
-  const [currency,setCurrency] = useState("inr");
-  const currencySymbol = currency==="inr"?"₹":currency==="eur"?"€":"$"
-  const changePage = (page)=>{
+  const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("inr");
+  const currencySymbol = currency === "inr" ? "₹" : currency === "eur" ? "€" : "$"
+  const changePage = (page) => {
     setPage(page);
     setLoading(true)
   }
+  const btns = new Array(132).fill(1)
   useEffect(() => {
     const fetchCoins = async () => {
       try {
@@ -28,9 +29,9 @@ const Coins = () => {
       }
     }
     fetchCoins();
-  }, [currency,page])
-  if(error){
-    return <ErrorComponent message = {"Error while Fetching Coins"}/>
+  }, [currency, page])
+  if (error) {
+    return <ErrorComponent message={"Error while Fetching Coins"} />
   }
   return (
     <Container maxW={"container.xl"}>
@@ -38,12 +39,17 @@ const Coins = () => {
         <HStack wrap={"wrap"}>
           {
             coins.map((i) => (
-              <CoinCard id={i.id} key={i.id} name={i.name} price={i.current_price} img={i.image} symbol={i.symbol} currencySymbol={currencySymbol}/>
+              <CoinCard id={i.id} key={i.id} name={i.name} price={i.current_price} img={i.image} symbol={i.symbol} currencySymbol={currencySymbol} />
             ))
           }
         </HStack>
-        <HStack>
-          <Button bgColor={"blackAlpha.900"} color={"white"} onClick={()=>changePage(2)}>2</Button>
+        <HStack w={"full"} overflowX={"auto"} p={8} >
+          {
+            btns.map((item, index) => (
+              <Button bgColor={"blackAlpha.900"} color={"white"} onClick={() => changePage(index+1)}>{index+1}</Button>
+
+            ))
+          }
         </HStack>
       </>
       )}
