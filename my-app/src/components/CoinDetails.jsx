850da976d18fccc747dfb5fd03312474ca636e20
AmoushRye <amoskhams1234@gmail.com>
2024-02-01 23:37:23 +0530

    date inserted

--- my-app/src/components/CoinDetails.jsx
+++ my-app/src/components/CoinDetails.jsx
@@ -1,4 +1,4 @@
-import { Container,Box } from '@chakra-ui/react'
+import { Container,Box, RadioGroup, Radio, HStack, VStack,Text } from '@chakra-ui/react'
 import React, { useState,useEffect } from 'react'
 import Loader from './Loader'
 import { useParams } from 'react-router-dom'
@@ -33,7 +33,20 @@ const CoinDetails = () => {
     <Container maxW={"container.xl"}>
       {loading?<Loader/>:(
         <>
-        <Box width={"full"} borderWidth={1}>laro</Box>
+        <Box width={"full"} borderWidth={1}>okk</Box>
+        {/* Button */}
+        <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
+          <HStack spacing={"4"}>
+            <Radio value={"inr"}>INR</Radio>
+            <Radio value={"usd"}>USD</Radio>
+            <Radio value={"eur"}>EUR</Radio>
+          </HStack>
+        </RadioGroup>
+        <VStack spacing={"4"} p={"16"} alignItems={"flex-start"}>
+          <Text fontSize={"small"} alignSelf={"center"} opacity={0.7}>
+            Last Updated on {Date().split("G")[0]}
+          </Text>
+        </VStack>
         </>
       )}
     </Container>
