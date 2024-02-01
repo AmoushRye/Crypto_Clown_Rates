40a57bae0e6c0de6f0ad540960b805fb6b756e75
AmoushRye <amoskhams1234@gmail.com>
2024-02-01 23:26:33 +0530

    use Params

--- my-app/src/components/CoinDetails.jsx
+++ my-app/src/components/CoinDetails.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import { Container,Box } from '@chakra-ui/react'
+import React, { useState,useEffect } from 'react'
+import Loader from './Loader'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { server } from '../index'
 
 const CoinDetails = () => {
+  const [coins, setCoins] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false);
+  const [currency, setCurrency] = useState("inr");
+  const params = useParams();
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const { data } = await axios.get(`${server}/coins/${params.id}`)
+        console.log(data)
+        setCoins(data)
+        setLoading(false)
+      } catch (error) {
+        setError(true)
+        setLoading(false);
+      }
+    }
+    fetchCoins();
+  }, [params.id])
   return (
-    <div>CoinDetails</div>
+    <Container maxW={"container.xl"}>
+      {loading?<Loader/>:(
+        <>
+        <Box width={"full"} borderWidth={1}>laro</Box>
+        </>
+      )}
+    </Container>
   )
 }
 
