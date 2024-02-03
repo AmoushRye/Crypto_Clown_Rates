7f6ef016bee5670de3b5e0e0b529eb628be9af75
AmoushRye <amoskhams1234@gmail.com>
2024-02-04 00:55:28 +0530

    values

--- my-app/src/components/CoinDetails.jsx
+++ my-app/src/components/CoinDetails.jsx
@@ -1,5 +1,5 @@
-import { Container,Box, RadioGroup, Radio, HStack, VStack,Text ,Image, Stat, StatLabel, StatHelpText, StatArrow} from '@chakra-ui/react'
-import React, { useState,useEffect } from 'react'
+import { Container, Box, RadioGroup, Radio, HStack, VStack, Text, Image, Stat, StatLabel, StatHelpText, StatArrow, Badge, Progress } from '@chakra-ui/react'
+import React, { useState, useEffect } from 'react'
 import Loader from './Loader'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
@@ -33,35 +33,70 @@ const CoinDetails = () => {
   }
   return (
     <Container maxW={"container.xl"}>
-      {loading?<Loader/>:(
+      {loading ? <Loader /> : (
         <>
-        <Box width={"full"} borderWidth={1}>okk</Box>
-        {/* Button */}
-        <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
-          <HStack spacing={"4"}>
-            <Radio value={"inr"}>INR</Radio>
-            <Radio value={"usd"}>USD</Radio>
-            <Radio value={"eur"}>EUR</Radio>
-          </HStack>
-        </RadioGroup>
-        <VStack spacing={"4"} p={"16"} alignItems={"flex-start"}>
-          <Text fontSize={"small"} alignSelf={"center"} opacity={0.7}>
-            Last Updated on {Date(coin.market_data.last_updated).split("G")[0]}
-          </Text>
-          <Image src={coin.image.large} w={"16"} h={"16"} objectFit={"contain"}/>
-          <Stat>
-            <StatLabel>{coin.name}</StatLabel>
-            <StatLabel>{currencySymbol}{coin.market_data.current_price[currency]}</StatLabel>
-            <StatHelpText>
-              <StatArrow type={coin.market_data.price_change_percentage_24h>0?"increase":"decrease"}/>
-              {coin.market_data.price_change_percentage_24h}%
-            </StatHelpText>
-          </Stat>
-        </VStack>
+          <Box width={"full"} borderWidth={1}>okk</Box>
+          {/* Button */}
+          <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
+            <HStack spacing={"4"}>
+              <Radio value={"inr"}>INR</Radio>
+              <Radio value={"usd"}>USD</Radio>
+              <Radio value={"eur"}>EUR</Radio>
+            </HStack>
+          </RadioGroup>
+          <VStack spacing={"4"} p={"16"} alignItems={"flex-start"}>
+            <Text fontSize={"small"} alignSelf={"center"} opacity={0.7}>
+              Last Updated on {Date(coin.market_data.last_updated).split("G")[0]}
+            </Text>
+            <Image src={coin.image.large} w={"16"} h={"16"} objectFit={"contain"} />
+            <Stat>
+              <StatLabel>{coin.name}</StatLabel>
+              <StatLabel>{currencySymbol}{coin.market_data.current_price[currency]}</StatLabel>
+              <StatHelpText>
+                <StatArrow type={coin.market_data.price_change_percentage_24h > 0 ? "increase" : "decrease"} />
+                {coin.market_data.price_change_percentage_24h}%
+              </StatHelpText>
+            </Stat>
+            <Badge fontSize={"2xl"} bgColor={"blackAlpha.800"} color={"white"}>
+              {`#${coin.market_cap_rank}`}
+
+            </Badge>
+            <CustomBar
+              high={`${currencySymbol}${coin.market_data.high_24h[currency]}`}
+              low={`${currencySymbol}${coin.market_data.low_24h[currency]}`}
+            />
+           <Box w={"full"} p="4">
+              <Item title={"Max Supply"} value={coin.market_data.max_supply} />
+              <Item
+                title={"Circulating Supply"}
+                value={coin.market_data.circulating_supply}
+              />
+              </Box>
+
+
+          </VStack>
         </>
       )}
     </Container>
   )
 }
+const Item = ({ title, value }) => (
+  <HStack justifyContent={"space-between"} w={"full"} my={"4"}>
+    <Text fontFamily={"Bebas Neue"} letterSpacing={"widest"}>
+      {title}
+    </Text>
+    <Text>{value}</Text>
+  </HStack>
+);
 
+const CustomBar = ({ high, low }) => (
+  <VStack w={"full"}>
+    <Progress value={50} colorScheme={"teal"} w={"full"} />
+    <HStack justifyContent={"space-between"} w={"full"}>
+      <Badge children={low} colorScheme={"red"} />
+      <Text fontSize={"sm"}>24H Range</Text>
+      <Badge children={high} colorScheme={"green"} />
+    </HStack>
+  </VStack>
+);
 export default CoinDetails
